6bf1a0a7d05570f4147b8fbca720994d70e0dfda
Sascha Duschen <Sascha@Saschas-MacBook-Pro.local>
2015-04-03 17:32:52 +0900

    sequelize create tables done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET teachers */
+router.get('/teachers', function(req, res) {
+  db.User.all({ include: [{ all: true }]}).then(function (result) {
+    res.send(JSON.stringify(result));
+  });
+});
+
+router.post('/teachers', function(req, res) {
+  console.log('TODO: implement edit/insert into teachers');
+  res.status(200).json(null);
 });
 
 module.exports = router;
